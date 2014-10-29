d9a8731a7b358641891bf2aee89060dd6ce094ae
Suresh Kumar D <sureshkumar_durairaj@yahoo.in>
2014-10-29 11:30:26 +0530

    POCEmployeeDetail - POC initialized.

---
+++ POCEmployeeDetail/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  POCEmployeeDetail
+//
+//  Created by Suresh on 29/10/14.
+//  Copyright (c) 2014 Neev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
